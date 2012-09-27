e3e29145a650c635e4d9307d229e8b1d45c2bd20
m.perillo <m.perillo@0fd5999a-94d0-4028-93f2-09691c278cb2>
2012-09-27 15:07:51 +0000

    * setted alias value (if present) instead of attribute name * production authorities.xml

--- src/main/java/eu/trentorise/smartcampus/ac/provider/adapters/AttributesAdapter.java
+++ src/main/java/eu/trentorise/smartcampus/ac/provider/adapters/AttributesAdapter.java
@@ -4,92 +4,100 @@
  */
 package eu.trentorise.smartcampus.ac.provider.adapters;
 
-import eu.trentorise.smartcampus.ac.provider.AcServiceException;
-import eu.trentorise.smartcampus.ac.provider.jaxbmodel.Authorities;
-import eu.trentorise.smartcampus.ac.provider.jaxbmodel.AuthorityMapping;
-import eu.trentorise.smartcampus.ac.provider.model.Authority;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.transform.stream.StreamSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import eu.trentorise.smartcampus.ac.provider.AcServiceException;
+import eu.trentorise.smartcampus.ac.provider.jaxbmodel.Attributes;
+import eu.trentorise.smartcampus.ac.provider.jaxbmodel.Authorities;
+import eu.trentorise.smartcampus.ac.provider.jaxbmodel.AuthorityMapping;
+import eu.trentorise.smartcampus.ac.provider.model.Authority;
+
 /**
- *
+ * 
  * @author Viktor Pravdin
  */
 @Component
 public class AttributesAdapter {
 
-    @Autowired
-    private AcServiceAdapter service;
-    private Map<String, AuthorityMapping> authorities;
+	@Autowired
+	private AcServiceAdapter service;
+	private Map<String, AuthorityMapping> authorities;
 
-    // Called from the AcServiceAdapter's init
-    protected void init() throws JAXBException, AcServiceException {
-        JAXBContext jaxb = JAXBContext.newInstance(AuthorityMapping.class,
-                Authorities.class);
-        Unmarshaller unm = jaxb.createUnmarshaller();
-        JAXBElement<Authorities> element = (JAXBElement<Authorities>) unm.
-                unmarshal(new StreamSource(getClass().getResourceAsStream(
-                "authorities.xml")), Authorities.class);
-        Authorities auths = element.getValue();
-        authorities = new HashMap<String, AuthorityMapping>();
-        for (AuthorityMapping mapping : auths.getAuthorityMapping()) {
-            Authority auth = service.getAuthorityByName(mapping.getName());
-            if (auth == null) {
-                auth = new Authority();
-                auth.setName(mapping.getName());
-                auth.setRedirectUrl(mapping.getUrl());
-                service.createAuthority(auth);
-            }
-            authorities.put(mapping.getName(), mapping);
-        }
-    }
+	// Called from the AcServiceAdapter's init
+	protected void init() throws JAXBException, AcServiceException {
+		JAXBContext jaxb = JAXBContext.newInstance(AuthorityMapping.class,
+				Authorities.class);
+		Unmarshaller unm = jaxb.createUnmarshaller();
+		JAXBElement<Authorities> element = (JAXBElement<Authorities>) unm
+				.unmarshal(
+						new StreamSource(getClass().getResourceAsStream(
+								"authorities.xml")), Authorities.class);
+		Authorities auths = element.getValue();
+		authorities = new HashMap<String, AuthorityMapping>();
+		for (AuthorityMapping mapping : auths.getAuthorityMapping()) {
+			Authority auth = service.getAuthorityByName(mapping.getName());
+			if (auth == null) {
+				auth = new Authority();
+				auth.setName(mapping.getName());
+				auth.setRedirectUrl(mapping.getUrl());
+				service.createAuthority(auth);
+			}
+			authorities.put(mapping.getName(), mapping);
+		}
+	}
 
-    public Map<String, String> getAttributes(String authority,
-            HttpServletRequest request) {
-        AuthorityMapping mapping = authorities.get(authority);
-        if (mapping == null) {
-            throw new IllegalArgumentException(
-                    "Unsupported authority: " + authority);
-        }
-        Map<String, String> attrs = new HashMap<String, String>();
-        for (String key : mapping.getIdentifyingAttributes()) {
-            Object value = request.getAttribute(key);
-            if (value != null) {
-                attrs.put(key, value.toString());
-            }
-        }
-        for (String key : mapping.getAttributes()) {
-            Object value = request.getAttribute(key);
-            if (value != null) {
-                attrs.put(key, value.toString());
-            }
-        }
-        return attrs;
-    }
+	public Map<String, String> getAttributes(String authority,
+			HttpServletRequest request) {
+		AuthorityMapping mapping = authorities.get(authority);
+		if (mapping == null) {
+			throw new IllegalArgumentException("Unsupported authority: "
+					+ authority);
+		}
+		Map<String, String> attrs = new HashMap<String, String>();
+		for (String key : mapping.getIdentifyingAttributes()) {
+			Object value = request.getAttribute(key);
+			if (value != null) {
+				attrs.put(key, value.toString());
+			}
+		}
+		for (Attributes attribute : mapping.getAttributes()) {
+			// used alias if present to set attribute in map
+			String key = (attribute.getAlias() != null && !attribute.getAlias()
+					.isEmpty()) ? attribute.getAlias() : attribute.getValue();
+			Object value = request.getAttribute(attribute.getValue());
+			if (value != null) {
+				attrs.put(key, value.toString());
+			}
+		}
+		return attrs;
+	}
 
-    public List<String> getIdentifyingAttributes(String authority) {
-        AuthorityMapping mapping = authorities.get(authority);
-        if (mapping == null) {
-            throw new IllegalArgumentException(
-                    "Unsupported authority: " + authority);
-        }
-        return mapping.getIdentifyingAttributes();
-    }
+	public List<String> getIdentifyingAttributes(String authority) {
+		AuthorityMapping mapping = authorities.get(authority);
+		if (mapping == null) {
+			throw new IllegalArgumentException("Unsupported authority: "
+					+ authority);
+		}
+		return mapping.getIdentifyingAttributes();
+	}
 
-    public Map<String, String> getAuthorityUrls() {
-        Map<String, String> map = new HashMap<String, String>();
-        for (AuthorityMapping mapping : authorities.values()) {
-            map.put(mapping.getName(), mapping.getUrl());
-        }
-        return map;
-    }
+	public Map<String, String> getAuthorityUrls() {
+		Map<String, String> map = new HashMap<String, String>();
+		for (AuthorityMapping mapping : authorities.values()) {
+			map.put(mapping.getName(), mapping.getUrl());
+		}
+		return map;
+	}
 }
--- src/main/java/eu/trentorise/smartcampus/ac/provider/controllers/AcSpWeb.java
+++ src/main/java/eu/trentorise/smartcampus/ac/provider/controllers/AcSpWeb.java
@@ -26,102 +26,118 @@ import eu.trentorise.smartcampus.ac.provider.adapters.AcServiceAdapter;
 import eu.trentorise.smartcampus.ac.provider.adapters.AttributesAdapter;
 
 /**
- *
+ * 
  * @author Viktor Pravdin
  */
 @Controller
 public class AcSpWeb {
 
-    @Autowired
-    private AcServiceAdapter service;
-    @Autowired
-    private AttributesAdapter attrAdapter;
-    @Value("${ac.redirect.hosts}")
-    private String redirectHosts;
-    private static String defaultHosts = null;
+	@Autowired
+	private AcServiceAdapter service;
+	@Autowired
+	private AttributesAdapter attrAdapter;
+	@Value("${ac.redirect.hosts}")
+	private String redirectHosts;
+	private static String defaultHosts = null;
 
-    @RequestMapping(method = RequestMethod.GET, value = "/getToken")
-    public String showAuthorities(Model model, HttpServletRequest request) {
-    	// FOR TESTING PURPOSES
-    	if (request.getParameter("TESTING") != null) {
-    		request.getSession().setAttribute("TESTING", true);
-    	}
-        Map<String, String> authorities = attrAdapter.getAuthorityUrls();
-        model.addAttribute("authorities", authorities);
+	@RequestMapping(method = RequestMethod.GET, value = "/getToken")
+	public String showAuthorities(Model model, HttpServletRequest request) {
+		// FOR TESTING PURPOSES
+		if (request.getParameter("TESTING") != null) {
+			request.getSession().setAttribute("TESTING", true);
+		}
+		Map<String, String> authorities = attrAdapter.getAuthorityUrls();
+		model.addAttribute("authorities", authorities);
 
-        String redirect = request.getParameter("redirect");
-        if (redirect != null && !redirect.isEmpty()) {
-        	if (!checkRedirect(redirect, redirectHosts, getDefaultHost(request))) {
-                throw new IllegalArgumentException("Incorrect redirect URL: " + redirect);
-        	}
-        	model.addAttribute("redirect",redirect);
-        } else {
-        	model.addAttribute("redirect","");
-        }
-        return "authorities";
-    }
+		String redirect = request.getParameter("redirect");
+		if (redirect != null && !redirect.isEmpty()) {
+			if (!checkRedirect(redirect, redirectHosts, getDefaultHost(request))) {
+				throw new IllegalArgumentException("Incorrect redirect URL: "
+						+ redirect);
+			}
+			model.addAttribute("redirect", redirect);
+		} else {
+			model.addAttribute("redirect", "");
+		}
+		return "authorities";
+	}
 
 	private static String getDefaultHost(HttpServletRequest request) {
 		if (defaultHosts == null) {
-			String result = request.getServerPort() == 80 ? (request.getServerName()+",") : "";
-			defaultHosts = result + request.getServerName()+":"+request.getServerPort();
-		} 
+			String result = request.getServerPort() == 80 ? (request
+					.getServerName() + ",") : "";
+			defaultHosts = result + request.getServerName() + ":"
+					+ request.getServerPort();
+		}
 		return defaultHosts;
 	}
 
-    private static boolean checkRedirect(String redirect, String redirectHosts, String _default) {
-    	String hosts = redirectHosts != null ? redirectHosts : _default;
-    	String[] array = hosts.split(",");
-    	for (String s : array) {
-    		if (redirect.matches("((https)|(http))://"+s+"/(.)*")) return true;
-    	}
+	private static boolean checkRedirect(String redirect, String redirectHosts,
+			String _default) {
+		String hosts = redirectHosts != null ? redirectHosts : _default;
+		String[] array = hosts.split(",");
+		for (String s : array) {
+			if (redirect.matches("((https)|(http))://" + s + "/(.)*"))
+				return true;
+		}
 		return false;
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/getToken/{authorityUrl}")
-    public String getToken(@PathVariable("authorityUrl") String authorityUrl,
-            HttpServletRequest request) throws AcServiceException {
-    	// FOR TESTING PURPOSES
-    	if (request.getParameter("TESTING") != null || request.getSession().getAttribute("TESTING") != null) {
-    		request.getSession().setAttribute("TESTING", true);
-    		Map<String,String> authorities = attrAdapter.getAuthorityUrls();
-    		for (String name : authorities.keySet()) {
-    			if (authorityUrl.equals(authorities.get(name))) {
-    				List<String> attrs = attrAdapter.getIdentifyingAttributes(name);
-    				for (String a : attrs) {
-    					request.setAttribute(a, "dummyvalue");
-    				}
-    			}
-    		}
-    	}
+	public String getToken(@PathVariable("authorityUrl") String authorityUrl,
+			HttpServletRequest request) throws AcServiceException {
+		// FOR TESTING PURPOSES
+		if (request.getParameter("TESTING") != null
+				|| request.getSession().getAttribute("TESTING") != null) {
+			request.getSession().setAttribute("TESTING", true);
+			Map<String, String> authorities = attrAdapter.getAuthorityUrls();
+			for (String name : authorities.keySet()) {
+				if (authorityUrl.equals(authorities.get(name))) {
+					List<String> attrs = attrAdapter
+							.getIdentifyingAttributes(name);
+					for (String a : attrs) {
+						request.setAttribute(a, "dummyvalue");
+					}
+					// set some values to test alias
 
-        String redirect = request.getParameter("redirect");
+					// attribute with alias : see authorities.xml FBK authority
+					request.setAttribute("Shib-name", "dummyvalue");
 
-    	String target = "/ac/success";
-        if (redirect != null && !redirect.isEmpty()) {
-        	if (!checkRedirect(redirect, redirectHosts,getDefaultHost(request))) {
-                throw new IllegalArgumentException("Incorrect redirect URL: " + redirect);
-        	}
-        	target = redirect;
-        }
-    	
-        String token = service.updateUser(authorityUrl, request);
-        return "redirect:"+target+"#" + token;
-    }
+					request.setAttribute("Shib-Application-ID", "dummyvalue");
 
-    @RequestMapping(method = RequestMethod.DELETE,
-    value = "/invalidateToken/{token}")
-    public void deleteToken(@RequestParam("token") String token) throws AcServiceException {
-        service.deleteToken(token);
-    }
+				}
+			}
+		}
 
-    @RequestMapping("/success")
-    public void success() {
-    }
+		String redirect = request.getParameter("redirect");
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    public void badRequest(IllegalArgumentException ex, HttpServletResponse resp) throws IOException {
-        resp.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
-    }
-    
+		String target = "/ac/success";
+		if (redirect != null && !redirect.isEmpty()) {
+			if (!checkRedirect(redirect, redirectHosts, getDefaultHost(request))) {
+				throw new IllegalArgumentException("Incorrect redirect URL: "
+						+ redirect);
+			}
+			target = redirect;
+		}
+
+		String token = service.updateUser(authorityUrl, request);
+		return "redirect:" + target + "#" + token;
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "/invalidateToken/{token}")
+	public void deleteToken(@RequestParam("token") String token)
+			throws AcServiceException {
+		service.deleteToken(token);
+	}
+
+	@RequestMapping("/success")
+	public void success() {
+	}
+
+	@ExceptionHandler(IllegalArgumentException.class)
+	public void badRequest(IllegalArgumentException ex, HttpServletResponse resp)
+			throws IOException {
+		resp.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
+	}
+
 }
